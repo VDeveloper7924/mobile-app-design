aa8a77faa9050c1ba736f90232b4d6923f394a51
DESKTOP-93B34CG\AAA <colinz7924@gmail.com>
2020-12-07 10:40:07 +0900

    modal

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,26 @@
-$('.page-search img').on('click', function(e) {
+$('.page-search img').mousedown( function(e) {
     e.preventDefault();
     console.log(e.which);
 
-    let imageUrl = e.target.getAttribute('src')
-    let targetImage = document.querySelector('.explorer .content-slidebar img');
-    targetImage.setAttribute('src', imageUrl);
-    $('.explorer').toggleClass('show-ex');
-    $('.page-search').hide()
-});
+    let imageUrl = e.target.getAttribute('src');
 
+    if (e.which === 1) {
+      let targetImage = document.querySelector('.explorer .content-slidebar img');
+      targetImage.setAttribute('src', imageUrl);
+      $('.explorer').toggleClass('show-ex');
+      $('.page-search').hide()
+    } else if (e.which === 3) {
+      let targetImage = document.querySelector('.modal-wrapper .model-image img');
+      targetImage.setAttribute('src', imageUrl);
+      $('.modal').addClass('is-visible');
+    }
+    else {
+      $('.modal').removeClass('is-visible');
+    }
+});
+$('body').on('click', function(e) {
+  $('.modal').removeClass('is-visible');
+});
 $('.explorer .back-arrow').on('click', function(e) {
   $('.page-search').show()
   $('.explorer').toggleClass('show-ex');
